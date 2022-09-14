5370c11f33bd19889a216ed3e0ae8f454dce0ad1
Remi Jonathan Choquette <contact@remijonathan.com>
2022-09-14 17:56:50 -0400

    Implemented dark mode

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,7 +6,7 @@ function loadIntro(title, text, image) {
         "</h1></div></div><div class=\"row\"><div class=\"col-md-5 align-left align-self-center\">" +
         "<img src=\"" +
         image +
-        "\" class=\"img-fluid\" alt=\"Luc\"></div><div class=\"col-md-2\"></div><div class=\"col-md-5 align-right align-self-start\"><p style='text-align: justify'>" +
+        "\" class=\"img-fluid \" alt=\"Luc\"></div><div class=\"col-md-2\"></div><div class=\"col-md-5 align-right align-self-start\"><p style='text-align: justify'>" +
         text +
         "</p></div></div></div>";
     //add intro to content
@@ -40,7 +40,7 @@ window.onload = function () {
 function addLogo() {
     let logo = document.createElement("div");
     logo.class = "logo";
-    logo.innerHTML = "<img src='static/media/logo.svg' alt='Luc'>";
+    logo.innerHTML = "<img src='static/media/logo.svg' id='logo' alt='Luc'>";
     logo.style.position = "fixed";
     logo.style.bottom = "0";
     logo.style.right = "0";
@@ -48,47 +48,72 @@ function addLogo() {
     document.body.appendChild(logo);
 }
 
-//function to add a floating nav button on the bottom center of page
+//function to add a floating nav button on the top left of page
 function addNavButton() {
     let navButton = document.createElement("div");
     navButton.class = "navButton";
     navButton.innerHTML = navHTML();
     navButton.style.position = "fixed";
-    navButton.style.bottom = "0";
-    navButton.style.left = "50%";
+    navButton.style.top = "0";
+    navButton.style.left = "0";
     navButton.style.zIndex = "1";
     document.body.appendChild(navButton);
 }
 
+
 function navHTML() {
-    return "<div class=\"container-fluid, nav-holder\">\n" +
-        "                <div class=\"row\">\n" +
-        "                    <nav class='menu'>\n" +
-        "                        <input class='menu-toggler' id='menu-toggler' type='checkbox'/>\n" +
-        "                        <label htmlFor='menu-toggler'></label>\n" +
-        "                        <ul>\n" +
-        "                            <li class='menu-item'>\n" +
-        "                                <a class='fa-regular fa-newspaper' href='Gallerie'></a>\n" +
-        "                            </li>\n" +
-        "                            <li class='menu-item'>\n" +
-        "                                <a class='fa-regular fa-address-card' href='CV'></a>\n" +
-        "                            </li>\n" +
-        "                            <li class='menu-item'>\n" +
-        "                                <a class='fa-regular fa-paper-plane' href='message'></a>\n" +
-        "                            </li>\n" +
-        "                            <li class='menu-item'>\n" +
-        "                                <a class='fa-brands fa-instagram' href='https://www.instagram.com/luc_dansero/' target='_blank'></a>\n" +
-        "                            </li>\n" +
-        "                        </ul>\n" +
-        "                    </nav>\n" +
-        "                </div>\n" +
-        "            </div>";
+    return "<section class=\"menu menu--circle\">\n" +
+        "  <input type=\"checkbox\" id=\"menu__active\"/>\n" +
+        "  <label for=\"menu__active\" class=\"menu__active\">\n" +
+        "    <div class=\"menu__toggle\">\n" +
+        "      <div class=\"icon\">\n" +
+        "        <div class=\"hamburger\"></div>\n" +
+        "      </div>\n" +
+        "    </div>\n" +
+        "    <input type=\"radio\" name=\"arrow--up\" id=\"degree--up-0\"/>\n" +
+        "    <input type=\"radio\" name=\"arrow--up\" id=\"degree--up-1\" />\n" +
+        "    <input type=\"radio\" name=\"arrow--up\" id=\"degree--up-2\" />\n" +
+        "    <div class=\"menu__listings\">\n" +
+        "      <ul class=\"circle\">\n" +
+        navItem("user", "#") +
+        navItem("moon-o", "javascript:toggleDarkMode()") +
+        navItem("cog", "#") +
+        navItem("", "#") +
+        navItem("", "#") +
+        navItem("", "#") +
+        navItem("", "#") +
+        navItem("envelope", "#") +
+        navItem("file", "#") +
+        navItem("picture-o", "#") +
+        "      </ul>\n" +
+        "    </div>\n" +
+        "    <div class=\"menu__arrow menu__arrow--top\">\n" +
+        "      <ul>\n" +
+        "        <li>\n" +
+        "          <label for=\"degree--up-0\"><div class=\"arrow\"></div></label>\n" +
+        "          <label for=\"degree--up-1\"><div class=\"arrow\"></div></label>\n" +
+        "          <label for=\"degree--up-2\"><div class=\"arrow\"></div></label>\n" +
+        "        </li>\n" +
+        "      </ul>\n" +
+        "    </div>\n" +
+        "  </label>\n" +
+        "</section>";
+}
+
+function navItem(icon, link) {
+    return "<li>\n" +
+        "          <div class=\"placeholder\">\n" +
+        "            <div class=\"upside\">\n" +
+        "              <a href=\"" + link + "\" class=\"button\"><i class=\"fa fa-" + icon + "\"></i></a>\n" +
+        "            </div>\n" +
+        "          </div>\n" +
+        "        </li>";
 }
 
 function fadeInContent() {
     //make loading text fade away using transform
     let loading = document.getElementById("content");
-    addLogo();
+    //addLogo();
     loading.style.transition = "opacity 1s";
     setTimeout(function () {
         document.getElementById("content").style.opacity = "1";
@@ -107,4 +132,12 @@ function lorem() {
         "ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu " +
         "fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
         "mollit anim id est laborum.";
+}
+
+//onclick toggle dark mode
+function toggleDarkMode() {
+    let body = document.body;
+    let logo = document.getElementById("logo");
+    body.classList.toggle("dark-mode");
+    logo.classList.toggle("dark-mode-logo");
 }
